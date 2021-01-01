b2173d46d2360905ea905dc705416abac49ba2e9
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2021-01-01 23:51:42 +0300

    Initial commit.

---
+++ src/test/java/com/github/vlsidlyarevich/pepeserver/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.github.vlsidlyarevich.pepeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
