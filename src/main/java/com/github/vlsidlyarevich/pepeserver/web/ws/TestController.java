98f9da5e08b42402dd10061b5a229290e2022a99
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2021-01-05 23:15:06 +0300

    WebSocket setup - test websocket plus simple web app added

---
+++ src/main/java/com/github/vlsidlyarevich/pepeserver/web/ws/TestController.java
@@ -0,0 +1,19 @@
+package com.github.vlsidlyarevich.pepeserver.web.ws;
+
+import com.github.vlsidlyarevich.pepeserver.web.ws.model.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class TestController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Message greeting(Message message) throws Exception {
+        log.info(message.getBody());
+        return new Message("Hello websocket");
+    }
+}
---
+++ src/main/java/com/github/vlsidlyarevich/pepeserver/web/ws/model/Message.java
@@ -0,0 +1,11 @@
+package com.github.vlsidlyarevich.pepeserver.web.ws.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Message {
+
+    private String body;
+}
